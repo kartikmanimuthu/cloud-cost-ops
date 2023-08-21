8ccae3800f8ba0671f8f67e402da6bd2211bddce
aditya-scrut <133007793+aditya-scrut@users.noreply.github.com>
2023-08-22 02:02:27 +0530

    sample form added

--- apps/web/app/dashboard/layout.tsx
+++ apps/web/app/dashboard/layout.tsx
@@ -8,14 +8,12 @@ export default function DashboardLayout({
 }) {
   return (
     <>
-      <div className="hidden md:block">
-        <div className="border-t">
-          <div className="bg-background">
-            <div className="grid lg:grid-cols-5">
-              <Sidebar playlists={playlists} className=" lg:block" />
-              {children}
-            </div>
+      <div className="bg-background">
+        <div className="flex">
+          <div className="w-64 fixed left-0 top-0 h-screen border-r">
+            <Sidebar playlists={playlists} className=" lg:block" />
           </div>
+          <div className="ml-64 flex-1">{children}</div>
         </div>
       </div>
     </>
--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -2,22 +2,22 @@
 
 import LineChartTabs from "./components/linecharts";
 import Assets from "./components/assets";
+import { DummyForm } from "./components/dummyForm";
 
 export default function Page() {
   return (
     <>
-      <div className="col-span-3 lg:col-span-4 lg:border-l">
+      <div className="col-span-3 lg:col-span-4 ">
         <div className="h-full px-4 py-6 lg:px-8">
           <div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 pt-6 gap-8">
-            <div>
+            <div className="grid gap-y-4">
               <LineChartTabs />
-            </div>
-            <div>
               <Assets />
             </div>
             <div>
-              <Assets />
+              <DummyForm />
             </div>
+            <div></div>
           </div>
         </div>
       </div>
